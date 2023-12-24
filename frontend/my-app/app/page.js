022c7e6ef35897644c83ecefe3f20b6c774fa0eb
Kubilay <kubikarabag@gmail.com>
2023-12-24 23:29:16 +0300

    navbar added

--- frontend/my-app/app/page.js
+++ frontend/my-app/app/page.js
@@ -1,9 +1,17 @@
 import Accident from "@/components/Accident/Accident";
 import MapContainer from "@/components/Accident/Map";
 import { ScrollAreaDemo } from "@/components/Accident/ScrollArea";
+import { NavigationMenuDemo } from "@/components/Navbar";
+
 import { Button } from "@/components/ui/button";
+
 import Image from "next/image";
 
 export default function Home() {
-  return <Accident></Accident>;
+  return (
+    <div>
+      <NavigationMenuDemo></NavigationMenuDemo>
+      <Accident></Accident>;
+    </div>
+  );
 }
